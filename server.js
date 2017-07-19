8b8d7b63923482309526e9b3d3775538ee3b2611
jasontuburan <jasontuburan@gmail.com>
2017-07-19 12:48:56 +0800

    pres repo for heroku 2

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(function(req, res,  next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port 3000' + PORT);
+  console.log('Express server is up on port' + PORT);
 });
